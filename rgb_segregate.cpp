63f7bb022b769a6bbc4772c2d5e32c25bcc124ad
Dudododo <a752601260@gmail.com>
2020-03-06 14:24:47 +0800

    exercise1

--- rgb_segregate.cpp
+++ rgb_segregate.cpp
@@ -2,9 +2,21 @@
 #include <iostream>
 
 using namespace cv;
+using namespace std;
 
 int main()
 {
-   
+	cv::Mat src_color = imread("1.png");
+	std::vector<cv::Mat> channels;
+	cv::split(src_color, channels);
+	cv::Mat B = channels.at(0);
+	cv::Mat G = channels.at(1);
+	cv::Mat R = channels.at(2);
+	cv::imshow("red", R);
+	cv::imshow("green", G);
+	cv::imshow("blue", B);
+	cv::imshow("original Mat", src_color);
+	waitKey(0);
+	return 0;
 }
 
